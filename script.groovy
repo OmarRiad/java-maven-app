eebd8c329cd52ca2bacf113e226cb485cf6501bc
Nana Janashia <nana.janashia@nnsoftware.at>
2023-06-01 13:14:05 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,19 @@
-def buildApp() {
+def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
+    }
 }
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 }
+
 return this
